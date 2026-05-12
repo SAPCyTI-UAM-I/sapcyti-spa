4f22bbeec1aedccd306c50d9db4687163250d4b3
Hans Diaz <hansdacc@gmail.com>
2026-05-12 01:35:35 -0600

    feat: primeNg, primeicons, ngx-translate agregados

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,20 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import Lara from '@primeuix/themes/lara';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+        options: {
+          darkModeSelector: false,
+        },
+      },
+    }),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardModule, ButtonModule, MessageModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
