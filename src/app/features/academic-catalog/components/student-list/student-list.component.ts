5d2abbbc205f9309d16d0a6774370896763644be
Hans Diaz <hansdacc@gmail.com>
2026-06-28 04:54:30 -0600

    feat: enhance student list component with reactive filter functionality and improved layout

--- src/app/features/academic-catalog/components/student-list/student-list.component.ts
+++ src/app/features/academic-catalog/components/student-list/student-list.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
@@ -6,7 +7,7 @@ import { Button } from 'primeng/button';
 import { InputText } from 'primeng/inputtext';
 import { Paginator } from 'primeng/paginator';
 import { Select } from 'primeng/select';
-import { Observable } from 'rxjs';
+import { debounceTime, merge, Observable } from 'rxjs';
 
 import { PageResponse, StudentCatalogItem } from '../../../../models';
 import { CopyableTextComponent, CatalogTagComponent } from '../../../../shared/components';
@@ -41,7 +42,7 @@ import { CatalogListBase } from '../catalog-list.base';
   ],
   templateUrl: './student-list.component.html',
 })
-export class StudentListComponent extends CatalogListBase<StudentCatalogItem> {
+export class StudentListComponent extends CatalogListBase<StudentCatalogItem> implements OnInit {
   private readonly fb = inject(NonNullableFormBuilder);
   private readonly service = inject(StudentService);
 
@@ -55,6 +56,23 @@ export class StudentListComponent extends CatalogListBase<StudentCatalogItem> {
   readonly statuses = CATALOG_STATUS_FILTER_OPTIONS;
   readonly programTypeTagSeverity = programTypeTagSeverity;
   readonly studentActiveTagSeverity = studentActiveTagSeverity;
+
+  override ngOnInit(): void {
+    super.ngOnInit();
+
+    merge(
+      this.filters.controls.search.valueChanges.pipe(debounceTime(300)),
+      this.filters.controls.programType.valueChanges,
+      this.filters.controls.active.valueChanges,
+    )
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => this.applyFilters());
+  }
+
+  override clearFilters(): void {
+    this.filters.reset({ search: '', programType: '', active: '' }, { emitEvent: false });
+    this.applyFilters();
+  }
 
   protected override fetchItems(): Observable<PageResponse<StudentCatalogItem>> {
     const filters = this.filters.getRawValue();
